090aeed81ba53160c4a2821f7dff6fe462840537
permabullnino <55150276+permabullnino@users.noreply.github.com>
2020-09-22 15:40:46 -0700

    binance

--- ALT SCANNING TOOLS/BINANCE_1.0_ADJVOL.py
+++ ALT SCANNING TOOLS/BINANCE_1.0_ADJVOL.py
@@ -19,7 +19,7 @@ api_secret = 'Jq2r0tvBv7QFEE72DObbWpRlvmSmJQIeSzFm4oAKNx3AW8mSOL17CM2wLt9yr0WU'
 client = Client(api_key, api_secret)
 
 # pull data
-coin = 'SOLBTC'
+coin = 'DCRBTC'
 date1 = "1 Jan, 2018"
 date2 = "22 Sep, 2020"
 
@@ -29,12 +29,18 @@ df.columns = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time
 df['Open Time'] = pd.to_datetime(df['Open Time'], unit='ms')    #format date
 df['Open'] = pd.to_numeric(df['Open'])
 df['Volume'] = pd.to_numeric(df['Volume'])
-
-print(df)
+df['Quote Asset Volume'] = pd.to_numeric(df['Quote Asset Volume'])
 
 # calc metrics
 
 df['adjvol'] = df['Volume'] / df['Number of Trades']
+df['btcadjvol'] = df['Quote Asset Volume'] / df['Number of Trades']
+
+df['cumvol'] = df['Volume'].cumsum()
+df['volshare'] = df['Volume'] / df['cumvol'].iloc[-1]
+df['volsum'] = df['volshare'].rolling(10).sum()
+
+print(df)
 
 # plot
 
@@ -48,11 +54,10 @@ ax1.set_facecolor('black')
 ax1.tick_params(color='w', labelcolor='w')
 ax1.grid()
 ax1.set_title(coin.upper(), fontsize=20, fontweight='bold', color='w')
-ax1.set_yscale('log')
 ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)))
 
-ax2 = plt.subplot(2,1,2, sharex=ax1)
-ax2.bar(df['Open Time'], df['adjvol'], color='aqua')
+ax2 = plt.subplot(2,1,2,sharex=ax1)
+ax2.bar(df['Open Time'], df['volsum'], color='aqua')
 ax2.set_facecolor('black')
 ax2.tick_params(color='w', labelcolor='w')
 ax2.grid()
--- ALT SCANNING TOOLS/BINANCE_2.0.py
+++ ALT SCANNING TOOLS/BINANCE_2.0.py
@@ -18,9 +18,42 @@ api_secret = 'Jq2r0tvBv7QFEE72DObbWpRlvmSmJQIeSzFm4oAKNx3AW8mSOL17CM2wLt9yr0WU'
 
 client = Client(api_key, api_secret)
 
-# pull data
+# pull data & format
 coin = 'DCRBTC'
 
-df = client.get_order_book(symbol=coin)
+ob = client.get_order_book(symbol=coin)
+bids = pd.DataFrame(ob['bids'])
+asks = pd.DataFrame(ob['asks'])
 
-print(df)
+bids.columns = [coin, 'Amount']
+asks.columns = [coin, 'Amount']
+
+bids[coin] = pd.to_numeric(bids[coin])
+asks[coin] = pd.to_numeric(asks[coin])
+
+bids['Amount'] = pd.to_numeric(bids['Amount'])
+asks['Amount'] = pd.to_numeric(asks['Amount'])
+
+# calc metrics
+
+bids['bidorderbook'] = bids['Amount'].cumsum()
+asks['askorderbook'] = asks['Amount'].cumsum()
+
+print(asks)
+
+# plot
+
+fig, ax1 = plt.subplots()
+fig.patch.set_facecolor('black')
+fig.patch.set_alpha(1)
+
+ax1.plot(asks[coin], asks['askorderbook'], color='r')
+ax1.plot(bids[coin], bids['bidorderbook'], color='lime')
+ax1.fill_between(asks[coin], asks['askorderbook'], where=asks['askorderbook'] > 0, facecolor='red', alpha=0.6)
+ax1.fill_between(bids[coin], bids['bidorderbook'], where=bids['bidorderbook'] > 0, facecolor='lime', alpha=0.6)
+ax1.set_facecolor('black')
+ax1.tick_params(color='w', labelcolor='w')
+ax1.grid()
+ax1.set_title(coin.upper() + " Orderbook", fontsize=20, fontweight='bold', color='w')
+
+plt.show()
