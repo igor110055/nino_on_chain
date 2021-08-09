64d647199ac45e2bf444b6914862a781d9e651ed
permabullnino <55150276+permabullnino@users.noreply.github.com>
2021-08-09 14:37:58 -0700

    nft final DF

--- DCR/NFT.py
+++ DCR/NFT.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import plotly.express as px
 
 """ endpoints = [assets,collections] """    #different endpoints which you plug to end of url
 
@@ -15,8 +16,8 @@ if offset == 0:
     querystring = {"offset":str(offset),"limit":str(limit),"asset_owner":owner}
     response = requests.request("GET", url, params=querystring)
     response = response.json()
-    df = pd.DataFrame(response)
-    #df.to_csv('nft.csv')
+    df2 = pd.DataFrame(response)
+    df2.to_csv('nft.csv')
 else:
     while offset >= 0:
         offset = offset - limit
@@ -25,21 +26,31 @@ else:
         response = response.json()
         response_list = pd.DataFrame(response)
         merge_list.append(response_list)
-    df = pd.concat(merge_list)
-    #df.to_csv('nft.csv')
+    df2 = pd.concat(merge_list)
+    df2.to_csv('nft.csv')
+print(df2)
 
 clean_list = []
 
-for item in df['stats']:
-    df2 = pd.DataFrame.from_dict(item, orient='index')
-    clean_list.append(df2)
-    print(df2)
+for item in df2['stats']:
+    df1 = pd.DataFrame.from_dict(item, orient='index')
+    clean_list.append(df1)
+    print(df1)
 
-df1 = pd.concat(clean_list,axis=1)
+df = pd.concat(clean_list,axis=1)
+df = df.transpose()
+df.reset_index(inplace=True,drop=True)
+df.insert(0, "name", df2['name'])
+#df1.to_csv('clean_nft.csv')
 
-for col in df1.columns:
-    df1.columns = df['name']
+print(df)
 
-df1.to_csv('clean_nft.csv')
+#Build Metrics
 
-print(df1)
+df['seven_day_trade_count'] = df['seven_day_volume'] / df['seven_day_average_price']
+df['seven_day_liquidity'] = df['seven_day_trade_count'] / df['total_supply']
+
+#Charting
+
+fig = px.bar(df, x='name', y='seven_day_liquidity', title='Stats for Collections Owned by ' + owner)
+fig.show()
